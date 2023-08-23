3e5db47baf4278dc96c3296f08ff4baffd88103d
Anukanksha Priya <102377700+cleveranu@users.noreply.github.com>
2023-08-23 09:17:47 +0530

    Update duplicates_in_string.java

--- String/Duplicate_in_string.java
+++ String/Duplicate_in_string.java
@@ -1,1 +1,37 @@
+import java.util.*;
+class duplicates_in_string
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        char ch;
+        System.out.println("Enter the string");
+        String s= sc.nextLine();
+        int l=s.length();
+        s=s.toLowerCase();
+        int temp[]=new int[26];
+        for(i=0;i<l;i++)
+        {
+            ch=s.charAt(i);
+             //  This is subtracting the ASCII value of 'a' from the ASCII value of the character stored in the variable ch. 
+             //This effectively maps the character to a zero-based index. For instance, if ch contains the character 'c', 
+             //then 'c' - 'a' would result in 2,because the ASCII value of 'c' is 99 and the ASCII value of 'a' is 97.
+            
+             
+                temp[ch-'a']++; 
+             
+        
 
+        }
+        System.out.println("Duplicate character present in string are");
+        for(int j=0;j<26;j++)
+        {
+            if(temp[j]>1)
+            {
+                System.out.println((char) ('a' + j) + ": " + temp[j]);
+            }
+        }
+
+    }
+}
